b3d598473ea00f846895e7cb7eecb752413ca83e
Xie Yuheng <xyheme@gmail.com>
2022-03-09 03:21:53 +0800

    - setup `models/user`

--- TODO.md
+++ TODO.md
@@ -1,2 +1,11 @@
+- `ro.createUser`
+- use `User` instead of `Ro` in commands
+- each user has his/her own dir
+
+- be able to login multiple accounts -- user `$RO_DIR/users`
+
+- `FileDownloadCommand`
+- `FileUploadCommand`
+
 - `PackageDownloadCommand` -- show progress
 - `PackageUploadCommand` -- show progress
---
+++ src/models/user.ts
@@ -0,0 +1,54 @@
+import axios from "axios"
+import os from "os"
+import Path from "path"
+import { Config } from "../config"
+import { LocalFileStore } from "../infra/local-file-store"
+
+export class User {
+  username: string
+  config: Config
+  local = new LocalFileStore(Path.resolve(os.homedir(), ".readonlylink"))
+
+  constructor(opts: { username: string; config: Config }) {
+    this.username = opts.username
+    this.config = opts.config
+  }
+
+  api(path: string): string {
+    return this.config.base_url + path
+  }
+
+  async readAllFiles(projectName: string): Promise<Record<string, string>> {
+    const token = await this.local.get("access-token")
+
+    const { data: files } = await axios.get(
+      this.api(`/files/${this.username}/${projectName}`),
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+
+    return files
+  }
+
+  async writeFiles(
+    projectName: string,
+    files: Record<string, string>
+  ): Promise<void> {
+    const token = await this.local.get("access-token")
+
+    await axios.patch(
+      this.api(`/files/${this.username}/${projectName}`),
+      { files },
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+  }
+
+  async createProjectIfNeed(projectName: string): Promise<void> {
+    const token = await this.local.get("access-token")
+
+    await axios.patch(
+      this.api(`/projects/${this.username}/${projectName}`),
+      "",
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+  }
+}
